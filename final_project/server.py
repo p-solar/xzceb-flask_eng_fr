1d528304cd07c831db79e7c8f59c006168683505
Prasanna Rotti <protti@enphaseenergy.com>
2023-01-04 00:14:41 -0800

    completed assign

--- final_project/server.py
+++ final_project/server.py
@@ -7,14 +7,14 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    frenchText = translator.english_to_french(textToTranslate)
+    return frenchText
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    englishText = translator.french_to_english(textToTranslate)
+    return englishText
 
 @app.route("/")
 def renderIndexPage():
